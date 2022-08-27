89f4c6b8e6a4956aabedc9b1581c1792960adc94
Nag46214 <97864157+Nag46214@users.noreply.github.com>
2022-08-27 11:25:39 +0530

    Create BubbleSortAlgorithm.java

---
+++ BubbleSortAlgorithm.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class BubbleSort{
+  //Bubble sort logic to implement sorting for the given array
+  public static int[] bubbleSort(int[]a)
+    {
+      for(int i=0;i<a.length;i++){
+        for(int j=i+1;j<a.length;j++){
+          if(a[i]>a[j]){
+            int temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+          }
+        }
+      }
+      return a;
+    }
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the length of the array ");
+    int n = sc.nextInt();
+    int []array = new int[n];
+    //for loop to add the elements to an array
+     for(int i=0;i<n;i++){
+        array[i]=sc.nextInt();  //array={9,7,4,1,2};
+    }
+    System.out.println(Arrays.toString(bubbleSort(array))); //output will be [1,2,4,7,9]
+  }
+}
