1ff15f37f91af226e1d9243274da0eba0dac7b48
Nag46214 <97864157+Nag46214@users.noreply.github.com>
2022-09-06 22:49:05 +0530

    Update BubbleSortAlgorithm.java

--- BubbleSortAlgorithm.java
+++ BubbleSortAlgorithm.java
@@ -5,10 +5,10 @@ class BubbleSort{
     {
       for(int i=0;i<a.length;i++){
         for(int j=i+1;j<a.length;j++){
-          if(a[i]>a[j]){
-            int temp=a[i];
-            a[i]=a[j];
-            a[j]=temp;
+          if(a[j]>a[j+1]){
+            int temp=a[j];
+            a[j]=a[j+1];
+            a[j+1]=temp;
           }
         }
       }
